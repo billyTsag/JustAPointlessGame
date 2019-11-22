c096270c70d345a33b084dbc35fc8942bd0665bb
Vasileios Tsagliotis <46362300+billyTsag@users.noreply.github.com>
2019-11-22 15:03:44 +0200

    Refactors_StartGame.cs

--- MyFirstPointlessGame/StartGame.cs
+++ MyFirstPointlessGame/StartGame.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,8 +8,7 @@ namespace MyFirstPointlessGame
     {
         internal static void AreYouReady(int _left, int _top)
         {
-            Console.WriteLine("Just a pointless game!\n\n\tIn this game you can only move your character by pressing the arrow buttons.\n\n\tBy pressing any other key your player will change from '*' to the pressed character.\n");
-            Console.WriteLine("\tPress the 'ESC' button at any time to exit the game!\n");
+            StartingText();
             string input;
             do
             {
@@ -21,8 +20,15 @@ namespace MyFirstPointlessGame
                 }
 
             } while (input != "y" && input != "n");
+            Console.Clear();
             Console.CursorVisible = false;
             Console.SetCursorPosition(_left, _top);
-        }        
+        }       
+        private static void StartingText() => 
+            Console.WriteLine
+            ("Just a pointless game!\n\n\t" +
+                "In this game you can only move your character by pressing the arrow buttons.\n\n\t" +
+                "By pressing any other key your player will change from '*' to the pressed character.\n\n\t" +
+                "Press the 'ESC' button at any time to exit the game!\n");
     }
 }
